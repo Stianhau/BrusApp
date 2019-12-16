e04d61968e21c84a48deea45dd1da56c33dc98ca
Stian <stian.hau@hotmail.no>
2019-12-16 14:58:37 +0100

    first commit

---
+++ app/src/main/java/com/example/brusapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.brusapp
+
+import android.opengl.Matrix
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sodaList = SodaList()
+
+
+
+        for(item in sodaList.ImageIdList){
+            val iv = ImageView(this)
+            iv.x = 20F
+            iv.y = 20F
+            iv.setImageResource(item)
+            iv.layoutParams = LinearLayout.LayoutParams(300,300)
+            Gallery.addView(iv)
+
+            val buttn = Button(this)
+        }
+        //setSupportActionBar(toolbar)
+    }
+/*
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/brusapp/SodaList.kt
@@ -0,0 +1,15 @@
+package com.example.brusapp
+
+class SodaList{
+
+    var ImageIdList = arrayOf<Int>(
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta,
+        R.drawable.fanta
+    )
+}
